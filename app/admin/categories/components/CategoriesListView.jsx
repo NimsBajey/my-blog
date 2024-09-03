227a87a0da2aba9a7914e9fc69764275502d7fde
Umesh Suwal <mail@umeshsuwal.com.np>
2024-09-03 20:37:54 +0545

    Update CategoriesListView.jsx

--- app/admin/categories/components/CategoriesListView.jsx
+++ app/admin/categories/components/CategoriesListView.jsx
@@ -54,7 +54,7 @@ export default function CategoriesListView() {
                         <td className="border px-4 py-2">{item?.slug}</td>
                         <td className="border px-4 py-2">
                             <Link href={`/admin/categories/form?id=${item?.id}`}>
-                                <button className="bg-blue-500 text-white rounded-full px-3 py-1 text-sm">Action</button>
+                                 <button className="bg-blue-300 font-semibold rounded-full px-5 py-2 text-sm">Edit</button>
                             </Link>
                         </td>
                     </tr>
